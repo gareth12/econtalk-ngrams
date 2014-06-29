0d574cf6ee0087e1d94464ac282cd4ce229e1fef
Gareth <gareth12@gmail.com>
2014-06-30 00:47:43 +0100

    frequency thresholds, fill zeros
    Better thresholds for ngram frequency, fill zeros in file names

--- ngrams.py
+++ ngrams.py
@@ -14,15 +14,16 @@ for file in os.listdir("./transcripts/"):
 input = cumulative.lower().translate(string.maketrans("",""), string.punctuation).split()
 
 # compute ngrams for each n
-for n in range(1,26):
+for n in range(1,23):
     output = {}
     for i in range(len(input)-n+1):
         g = ' '.join(input[i:i+n])
         output.setdefault(g, 0)
         output[g] += 1
     sorted_output = list(reversed(sorted(output.iteritems(), key=operator.itemgetter(1))))
-    # save top 1000 entries to a file
-    entries = min(1000,len(sorted_output))
-    w = csv.writer(open("ngrams/ngrams"+str(n)+".csv", "w"))
-    for j in sorted_output[0:entries]:
-        w.writerow([j[1], j[0]])
+    # save entries to file
+    w = csv.writer(open("ngrams/ngrams"+str(n).zfill(2)+".csv", "w"))
+    j=0
+    while j<len(sorted_output) and sorted_output[j][1]>max(3,0.001*sorted_output[0][1]):
+        w.writerow([sorted_output[j][1], sorted_output[j][0]])
+        j=j+1
